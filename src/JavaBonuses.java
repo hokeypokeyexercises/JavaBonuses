957c1cc8ba584b68b05f140a596a926063934a04
Tony Clayton <te.clayton@yahoo.com>
2019-03-22 15:43:37 -0500

    exercise bonus 4,5,6 added

--- src/JavaBonuses.java
+++ src/JavaBonuses.java
@@ -1,11 +1,13 @@
+package src;
+
 import java.util.Scanner;
 import java.lang.*;
 import java.io.*;
 import java.util.*;
 
-public class JavaBonus {
+public class JavaBonuses {
     public static void main(String[] args) {
-    
+
 //        String string = "Here is a test string";
 //
 //        StringBuilder reverse = new StringBuilder();
