cbb6cb6715939d15f71dabe83e3214bbcc94f7ab
Parick Cowan <patrick.w.cowan@gmail.com>
2019-03-29 14:16:39 -0500

    added 2 of Patricks Exercises

--- src/JavaBonuses.java
+++ src/JavaBonuses.java
@@ -1,8 +1,18 @@
 package src;
 
-import java.util.Scanner;
-
 public class JavaBonuses {
+
+//    public static void comboString(String shorter, String longer) {
+//        if (shorter.length() < longer.length()) {
+//            System.out.printf("%s%s%s\n",shorter,longer,shorter);
+//        } else if (longer.length() < shorter.length()) {
+//            System.out.printf("%s%s%s\n",longer,shorter,longer);
+//        }
+//
+//
+//
+//    }
+
     public static void main(String[] args) {
 
 //        String string = "Here is a test string";
@@ -33,24 +43,21 @@ public class JavaBonuses {
 //            System.out.println("The string is a palindrome.");
 //        else
 //            System.out.println("The string isn't a palindrome.");
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Enter a string: ");
+//        String input = scanner.next();
+//        System.out.println("Enter a second string: ");
+//        String input2 = scanner.next();
+//
+//        if ((input.substring(input.length() -1).equals(input2.substring(0, 1)))) {
+//            input = input.substring(0, input.length() - 1);
+//            System.out.println(input + input2);
+//
+//        } else
+//            System.out.println(input + input2);
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter a string: ");
-        String input = scanner.next();
-        System.out.println("Enter a second string: ");
-        String input2 = scanner.next();
-
-        if ((input.substring(input.length() -1).equals(input2.substring(0, 1)))) {
-            input = input.substring(0, input.length() - 1);
-            System.out.println(input + input2);
-
-        } else
-            System.out.println(input + input2);
-
-
-    }
-}
 
 
 //        4) Read in a string and write out the reverse string: Java String -> gnirtS avaJ
@@ -67,20 +74,34 @@ public class JavaBonuses {
 //        conCat('abc', '') → abc
 
 
+//Patrick's Labs
+
 //        1) create three variables, start, end, and sum. Use a loop to find the summation of
 //        all the numbers between start and end (inclusive)
+
+//            int start = 0;
+//            int end = 20;
+//            int sum = 0;
 //
+//            for (int i = start; i <= end; i++) {
+//                sum += i;
+//            }
 //
-//
+//            System.out.println(sum);
+
+
+
+
+
 //        3) output a table representing the binary representations of the powers of 2
 //        decimal       |       binary
-//        1     |     00000001
-//        2     |     00000010
-//        4     |     00000100 (edited)
-//
-//
-//
-//
+//        1             |     00000001
+//        2             |     00000010
+//        4             |     00000100 (edited)
+
+
+
+
 //        7) Given 2 strings, a and b, return a string of the form short+long+short, with the shorter
 //        string on the outside and the longer string on the inside. The strings will not be the same length,
 //        but they may be empty (length 0).
@@ -88,3 +109,17 @@ public class JavaBonuses {
 //        comboString('Hello', 'hi') → hiHellohi
 //        caomboString('Hi', 'Hello') → HiHelloHi
 //        comboString('aaa', 'b') → baaab
+
+
+//        comboString("hello","hi");
+//        comboString("Hi","Hello");
+//        comboString("aaa","b");
+
+
+
+
+
+
+
+    }
+}
